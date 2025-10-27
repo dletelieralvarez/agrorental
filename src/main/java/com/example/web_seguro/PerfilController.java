19575f8ef9ea090e0b2dccf9bb4691abf41956e9
mautapiao <mau.tapiao@duocuc.cl>
2025-10-26 23:56:56 -0300

    Integracion de empresa maquinaria y pefil mtapia

---
+++ src/main/java/com/example/web_seguro/PerfilController.java
@@ -0,0 +1,18 @@
+package com.example.web_seguro;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PerfilController {    
+
+   @GetMapping("/perfil")
+    public String getPerfil(@RequestParam(name="name", required = false, defaultValue = "Seguridad y Calidad en el Desarrollo") String name,
+                        Model model) {
+        model.addAttribute("name", name);
+        return "perfil";
+    }
+
+}
